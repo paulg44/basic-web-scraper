40773bce134029abb3b1b883ca73a551f237f410
Paul Garton <paulgarton1984@hotmail.com>
2024-09-27 20:57:00 +0100

    attempt to add variable to frontend: need to add a server and fetch request

--- playwright_scraper/playwrightScraper.js
+++ playwright_scraper/playwrightScraper.js
@@ -1,4 +1,3 @@
-const { url } = require("inspector");
 const playwright = require("playwright");
 
 async function scrapeWebsiteURL(url) {
@@ -10,9 +9,16 @@ async function scrapeWebsiteURL(url) {
 
   await page.goto(url);
 
+  // const container = page.locator("_ac7v xras4av xgc1b0m xat24cr xzboxd6");
+  // const link = page.getByRole("link");
+
+  // await page.click(link);
+
   const result = page.url();
 
   console.log(result);
+
+  await browser.close();
   return result;
 }
 
@@ -20,7 +26,11 @@ scrapeWebsiteURL("https://www.instagram.com/p/C__B59Vgd0T/?hl=pa")
   .then((data) => {
     const substringFromURL = data.substring(data.length - 17);
     const urlResult = substringFromURL.substring(0, 11);
-    console.log(urlResult);
+    console.log(urlResult, data);
+
+    const variableText = document.createElement("p");
+    variableText.innerHTML = "testing";
+    document.body.appendChild(variableText);
   })
   .catch((e) => {
     console.error(e);
